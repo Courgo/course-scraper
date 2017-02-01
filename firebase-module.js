f28e03e8af80f30ef284a3c0709945d639c2ec1e
Chris Li <chris@city365.ca>
2017-02-01 15:17:03 -0800

    Implemented logic to scrape a university course directory and write the data to Firebase.

--- firebase-module.js
+++ firebase-module.js
@@ -2,12 +2,51 @@
 
 var admin = require('firebase-admin');
 
+var firebaseApp;
+
 function initialize(pathToCredentials, dbUrl) {
-    var fbApp = admin.initializeApp({
+    firebaseApp = admin.initializeApp({
         credential: admin.credential.cert(require(pathToCredentials)),
         databaseURL: dbUrl
     });
-    module.exports.firebaseDb = fbApp.database();
+    module.exports.firebaseDb = firebaseApp.database();
 }
-
 module.exports.initialize = initialize;
+
+function saveCourses(universityVal, courses, res) {
+    if (!firebaseApp) {
+        console.log('Firebase app has not been initialized yet.');
+        return;
+    }
+    var coursesRef = firebaseApp.database().ref('courses');
+    coursesRef
+        .orderByChild('university')
+        .equalTo(universityVal)
+        .once('value')
+        .then(function(snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                var courseName = childSnapshot.val().course_name;
+                // remove all courses from the local course array that have
+                // already been saved to the database.
+                var courseHasBeenAdded = courses.includes(courseName);
+                if (courseHasBeenAdded) {
+                    var index = courses.indexOf(courseName);
+                    if (index > -1) {
+                        courses.splice(index, 1);
+                    }
+                }
+            });
+            // save remaining courses to the database
+            for (var i = 0; i < courses.length; i++) {
+                var newCourseRef = coursesRef.push();
+                newCourseRef.set({
+                    course_name: courses[i],
+                    university: universityVal
+                });
+            }
+            res.json({
+                "success": true
+            });
+        });
+}
+module.exports.saveCourses = saveCourses;
--- routes/ubc.js
+++ routes/ubc.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var request = require('request');
+var request = require('request-promise');
 var cheerio = require('cheerio');
+var q = require('bluebird');
+var firebase = require('../firebase-module');
 
-/* Start scraping. */
+/* Custom scraping logic for UBC's course directory. */
 router.get('/', function(req, res, next) {
     request('https://courses.students.ubc.ca/cs/main?pname=subjarea', function(error, response, html) {
         if (!error && response.statusCode == 200) {
@@ -11,14 +13,46 @@ router.get('/', function(req, res, next) {
             var subjectTable = $('#mainTable').find('tbody').find('tr');
             var subjects = [];
             subjectTable.each(function(i, element) {
-                console.log($(this).text());
+                var row = $(this).children().first();
+                var subject = row.find('a');
+                if (subject.length !== 0) {
+                    subjects.push(subject.text());
+                }
             });
 
-            res.json({
-                "subjects": subjects
-            });
+            // navigate to each subject's course list and grab
+            // the list of courses
+            var subjectUrl = "https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=1&dept=";
+            var courseRequests = [];
+            for (var i = 0; i < subjects.length; i++) {
+                var courseRequest = request({
+                    uri: subjectUrl + subjects[i],
+                    transform: function(body) {
+                        return cheerio.load(body);
+                    }
+                }).then(subjectHandler);
+                courseRequests.push(courseRequest);
+            }
+
+            q.all(courseRequests).then(function(results) {
+                var courses = [].concat.apply([], results);
+                firebase.saveCourses("ubc", courses, res);
+            })
         }
     });
 });
+
+function subjectHandler($) {
+    var courses = [];
+    var courseTable = $('#mainTable').find('tbody').find('tr');
+    courseTable.each(function(i, element) {
+        var row = $(this).children().first();
+        var course = row.find('a');
+        if (course.length !== 0) {
+            courses.push(course.text());
+        }
+    });
+    return courses;
+}
 
 module.exports = router;
